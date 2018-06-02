2d3f4d985219ae6322045950c6f1778ca72cf49e
Star Lord <altamashymomin@gmail.com>
2018-06-02 19:57:44 -0400

    3rd

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,54 +1,55 @@
 $(document).ready(function(){
 
 
-function crystalGenerator() {
+var computerPick = function crystalGenerator() {
     var gem = Math.floor(Math.random() * (120 - 19 +1)) + 19;
     return gem;
 }
 
-function gemGenerator() {
-    var gem1 = Math.floor((Math.random()*12)+1);
-    return gem1;
-}
-
-function gem2Generator() {
-    var gem2 = Math.floor((Math.random()*12)+1);
-    return gem2;
-}
-
-function gem3Generator() {
-    var gem3 = Math.floor((Math.random()*12)+1);
-    return gem3;
-}
-
-function gem4Generator() {
-    var gem4 = Math.floor((Math.random()*12)+1);
-    return gem4;
-}
-
-//console.log(gem1Generator());
-//console.log(gem2Generator());
-//console.log(gem3Generator());
-//console.log(gem4Generator());
+console.log(crystalGenerator());
 
 var counter = 0;
 var wins = 0;
 var losses = 0;
 var numberSoFar = 0;
 var randomNum = 0;
+
 $("#wins").text(wins);
 $("#losses").text(losses);
 
 $("#randomNum").text(crystalGenerator());
 
+gem1 = Math.floor((Math.random()*12)+1);
+    console.log(gem1);
+gem2 = Math.floor((Math.random()*12)+1);
+    console.log(gem2);
+gem3 = Math.floor((Math.random()*12)+1);
+    console.log(gem3);
+gem4 = Math.floor((Math.random()*12)+1);
+    console.log(gem4);
 
-$("#gem1").html("<img src=" + "assets/images/gem1.png" + " value=" + gemGenerator + " class='container'" + ">");
-$("#gem2").html("<img src=" + "assets/images/gem2.png" + " value=" + gemGenerator + " class='container'" + ">");
-$("#gem3").html("<img src=" + "assets/images/gem3.png" + " value=" + gemGenerator + " class='container'" + ">");
-$("#gem4").html("<img src=" + "assets/images/gem4.png" + " value=" + gemGenerator + " class='container'" + ">");
+$("#gem1").html("<img src=" + "assets/images/gem1.png" + " value=" + gem1 + " class='container'" + ">");
+$("#gem2").html("<img src=" + "assets/images/gem2.png" + " value=" + gem2 + " class='container'" + ">");
+$("#gem3").html("<img src=" + "assets/images/gem3.png" + " value=" + gem3 + " class='container'" + ">");
+$("#gem4").html("<img src=" + "assets/images/gem4.png" + " value=" + gem4 + " class='container'" + ">");
 
+$("gem").on("click", function(){
+    var newNumbSoFar = numberSoFar += parseInt($(this).attr("value"));
+    $("#currentScore").html(newNumbSoFar);
 
+    if (newNumbSoFar === computerPick) {
+        wins++ ;
+        $("#wins").html(wins);
+    }
+
+    else if (newNumbSoFar > computerPick) {
+        losses++ ;
+        $("#losses").html(losses);
+    }
+    
 })
+
+
 
 
 
