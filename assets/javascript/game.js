7717e522d5feab073a684c2b6d50a4685da68e90
Star Lord <altamashymomin@gmail.com>
2018-06-02 20:44:54 -0400

    weird error at line 9 that prevents the game from restarting

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,22 @@
 $(document).ready(function(){
 
+var gem = Math.floor(Math.random() * (120 - 19 +1)) + 19;
+    console.log(gem);
 
-function crystalGenerator() {
-    var gem = Math.floor(Math.random() * (120 - 19 +1)) + 19;
-    return gem;
-}
 
 function reset() {
-    $("#randomNum").html(crystalGenerator());
+   numberSoFar = 0;
+   $("#currentScore").html("Current Score: " + ds); 
 
-    numberSoFar = 0;
-    $("#currentScore").html("Current score: " + numberbSoFar);
+   randomNum = 0;
+   $("#randomNum").html("Computer's pick: " + gem);
 
-    wins = 0;
-    $("#wins").html("Wins: " + wins);
-
-    losses = 0;
-    $("#losses").html("Losses: " + losses);
-
-    gem1 = Math.floor((Math.random()*12)+1);
+   gem1 = Math.floor((Math.random()*12)+1);
     console.log(gem1);
-
     gem2 = Math.floor((Math.random()*12)+1); 
     console.log(gem2);
-
     gem3 = Math.floor((Math.random()*12)+1);
     console.log(gem3);
-
     gem4 = Math.floor((Math.random()*12)+1);
     console.log(gem4);
 
@@ -34,21 +24,19 @@ $("#gem1").html("<img src=" + "assets/images/gem1.png" + " value=" + gem1 + " cl
 $("#gem2").html("<img src=" + "assets/images/gem2.png" + " value=" + gem2 + " class='container'" + ">");
 $("#gem3").html("<img src=" + "assets/images/gem3.png" + " value=" + gem3 + " class='container'" + ">");
 $("#gem4").html("<img src=" + "assets/images/gem4.png" + " value=" + gem4 + " class='container'" + ">");
-
+   
 }
 
-console.log(crystalGenerator());
 
-var counter = 0;
 var wins = 0;
 var losses = 0;
 var numberSoFar = 0;
 var randomNum = 0;
 
-$("#wins").text("Wins: " + wins);
-$("#losses").text("Losses: " + losses);
-
-$("#randomNum").text(crystalGenerator());
+$("#wins").html("Wins: " + wins);
+$("#losses").html("Losses: " + losses);
+$("#currentScore").html("Current Score: " + numberSoFar);
+$("#randomNum").html("Computer's pick: " + gem);
 
 gem1 = Math.floor((Math.random()*12)+1);
     console.log(gem1);
@@ -67,15 +55,15 @@ $("#gem4").html("<img src=" + "assets/images/gem4.png" + " value=" + gem4 + " cl
 $("img").on("click", function(){
     var newNumbSoFar = numberSoFar += parseInt($(this).attr("value"));
     $("#currentScore").html("Current Score: " + newNumbSoFar);
-
-    if (newNumbSoFar === crystalGenerator()) {
+    
+    if (newNumbSoFar === gem) {
         wins++ ;
         $("#wins").html("Wins: " + wins);
         alert("You win!");
         reset();
     }
 
-    else if (newNumbSoFar > crystalGenerator()) {
+    else if (newNumbSoFar > gem) {
         losses++ ;
         $("#losses").html("Losses: " + losses);
         alert("You've went over!");
